df5ca97558db8b32d7c4a50693230b5241d4544c
derekyhsieh <devderekhsieh@gmail.com>
2024-04-27 17:48:53 -0700

    videot

--- app/page.tsx
+++ app/page.tsx
@@ -17,54 +17,71 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 import { helix } from "ldrs";
 import { CustomCard } from "@/components/CustomCard";
 
-const testData = {
-  status: "success",
-  articles: [
-    {
-      id: "3097fca9b1ec8942c4305e550ef1b50a",
-      title: "China's first Sora-level text-to-video large model Vidu unveiled",
-      description:
-        "China's first Sora-level text-to-video large model Vidu was unveiled at the 2024 Zhongguancun Forum in Beijing on Saturday, intensifying the artificial intelligence competition globally.",
-      metadata: {
-        date: 1714253228.120358,
-        quality: 0.9,
-        source: "https://github.com",
-        tags: [
-          "ai",
-          "generative ai",
-          "open ai",
-          "sora",
-          "video generation",
-          "vidu",
-          "china",
-          "china ai",
-          "text to video",
-        ],
-        text: "China's first Sora-level text-to-video large model Vidu was unveiled at the 2024 Zhongguancun Forum in Beijing on Saturday, intensifying the artificial intelligence competition globally.",
-        type: "https://github.com",
-      },
-      match: {
-        china: 1.0008558,
-      },
-    },
-  ],
-};
-
 export default function Home() {
   const user = useUser();
 
   const [loading, setLoading] = useState(false);
   const [tags, setTags] = useState<string[]>([]);
 
-  const [data, setData] = useState(testData);
+  const [data, setData] = useState(null);
 
   const onSubmit = async () => {
     setLoading(true);
 
     console.log("getting newsletter", tags);
+    await getNewsletter(tags);
   };
 
-  const getNewsletter = async () => {};
+  const getNewsletter = async (tags: string[]) => {
+    const currentDate = Math.floor(Date.now() / 1000); // Get current timestamp
+    const twoWeeksAgo = currentDate - 14 * 24 * 60 * 60; // Get timestamp 2 weeks ago
+
+    const requestBody = {
+      tags: tags.map((tag) => tag.text.toLowerCase()),
+      date: twoWeeksAgo,
+    };
+
+    // hit supabase update user id
+
+    // replace user_profiles id user id, update tags with new tags
+    // console.log(user.id)
+    // const { data, error } =  await supabase.from("user_profiles").upsert({
+    //   id: user.id,
+    //   tags: tags.map((tag) => tag.text.toLowerCase()),
+    // });
+
+    // console.log(error)
+
+    // console.log(data);
+
+    console.log(JSON.stringify(requestBody));
+
+    try {
+      const response = await fetch(
+        "https://pearx-backend.fly.dev/get_articles",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestBody),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+        console.log("DATA", data);
+        setLoading(false);
+      } else {
+        console.error("Error fetching articles:", response.statusText);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error("Error fetching articles:", error);
+      setLoading(false);
+    }
+  };
 
   helix.register();
 
@@ -89,60 +106,96 @@ export default function Home() {
 const NewsletterView = ({ data }) => {
   return (
     <div className="flex-grow flex flex-col items-center mt-5 space-y-5">
-      <h1 className="text-4xl font-bold">Derek's Newsletter</h1>
+      <h1 className="text-4xl font-bold mb-3">Derek's Newsletter</h1>
 
-      <Tabs defaultValue="articles" className="items-center justify-center w-1/2">
+      <Tabs
+        defaultValue="articles"
+        className="items-center justify-center w-1/2"
+      >
         <TabsList>
           <TabsTrigger value="articles">News Articles</TabsTrigger>
           <TabsTrigger value="research">Research Papers</TabsTrigger>
           <TabsTrigger value="github">GitHub Repos</TabsTrigger>
         </TabsList>
         <TabsContent value="articles">
-          <div className="">
-            {data.articles.map((article: any) => {
-              return (
-                <CustomCard
-                  key={article.id}
-                  title={article.title}
-                  description={article.description}
-                  link={article.metadata.source}
-                  tags={article.metadata.tags}
-                  date={article.metadata.date}
-                />
-              );
-            })}
+          <div className="space-y-5 mt-4">
+            {data.articles
+
+              .filter(
+                (article: any) =>
+                  !article.metadata.source.includes("arxiv") &&
+                  !article.metadata.source.includes("github")
+              )
+              .slice(0, 5)
+              .map((article: any) => {
+                return (
+                  <CustomCard
+                    key={article.id}
+                    title={article.metadata.title}
+                    description={article.metadata.description}
+                    link={article.metadata.source}
+                    tags={article.metadata.tags}
+                    date={article.metadata.date}
+                  />
+                );
+              })}
           </div>
         </TabsContent>
         <TabsContent value="research">
-          <div className="">
-            {data.articles.map((article: any) => {
-              return (
-                <CustomCard
-                  key={article.id}
-                  title={article.title}
-                  description={article.description}
-                  link={article.metadata.source}
-                  tags={article.metadata.tags}
-                  date={article.metadata.date}
-                />
-              );
-            })}
+          <div className="space-y-5 mt-4">
+            {data.articles
+              .filter((article: any) =>
+                article.metadata.source.includes("arxiv")
+              )
+              .slice(0, 5)
+              .map((article: any) => {
+                return (
+                  <CustomCard
+                    key={article.id}
+                    title={article.metadata.title}
+                    description={
+                      article.metadata.description.length > 100
+                        ? `${article.metadata.description.substring(0, 100)}...`
+                        : article.metadata.description
+                    }
+                    link={article.metadata.source}
+                    tags={article.metadata.tags}
+                    date={article.metadata.date}
+                  />
+                );
+              })}
           </div>
         </TabsContent>
         <TabsContent value="github">
-          <div className="">
-            {data.articles.map((article: any) => {
-              return (
-                <CustomCard
-                  key={article.id}
-                  title={article.title}
-                  description={article.description}
-                  link={article.metadata.source}
-                  tags={article.metadata.tags}
-                  date={article.metadata.date}
-                />
-              );
-            })}
+          <div className="space-y-5 mt-4">
+            {data.articles
+              .filter((article: any) =>
+                article.metadata.source.includes("github")
+              )
+              .slice(0, 6)
+              .map((article: any) => {
+                if (article.metadata.title === "软件开发管理与程序员培养") {
+                  null
+                } else {
+                  return (
+                    <CustomCard
+                      key={article.id}
+                      title={article.metadata.title}
+                      description={
+                        article.metadata.description.length > 100
+                          ? `${article.metadata.description.substring(
+                              0,
+                              100
+                            )}...`
+                          : article.metadata.description
+                      }
+                      link={article.metadata.source}
+                      tags={article.metadata.tags}
+                      date={article.metadata.date}
+                    />
+                  );
+                }
+              })}
           </div>
         </TabsContent>
       </Tabs>
